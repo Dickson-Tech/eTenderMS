f19f80b30107b06ad6f715c80d394edc290f8c3c
Dickson NENGA <128114861+Dickson-Tech@users.noreply.github.com>
2025-04-07 23:34:11 +1000

    Update urls.py

--- NpcProject/urls.py
+++ NpcProject/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from eTender import views
+from core import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('add-need/', views.add_need, name='add_need'),
+    path('advertise-contract/', views.advertise_contract, name='advertise_contract'),
 ]
